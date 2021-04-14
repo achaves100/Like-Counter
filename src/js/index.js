e8da821ee1e12781a88a054891a036b384559406
achaves100 <adan.chaves@outlook.com>
2021-04-14 22:49:50 +0000

    udpate

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,20 @@ import { Home } from "./component/home.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+import react, { useState } from "react";
+import reactDom from "react-dom";
+import "././styles.css";
+
+const Counter = () => {
+	const [count, setCount] = useState(0);
+	return;
+	<div>
+		<h2>{count} likes</h2>
+		<span OnClick={() => setCount(count + 1)}>👍🏽</span>
+		<span OnClick={() => setCount(count - 1)}>👎🏽</span>
+		<h3>Like or dislike to increase/decrease</h3>
+	</div>;
+};
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
